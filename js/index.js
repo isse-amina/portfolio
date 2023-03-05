97a35b737895a5f1ffe139368d1ced042eb89ea0
isse-amina <110896537+isse-amina@users.noreply.github.com>
2023-03-05 15:59:24 -0500

    modify to open links in new tab
    git commit -m "modify to open links in new tab"

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 const contactMe = document.querySelectorAll(".contact-me");
 contactMe.forEach((item, index) => {
     item.addEventListener("click", arrow => {
-        location.href = "https://www.linkedin.com/in/amina-isse/"; 
+        window.open("https://www.linkedin.com/in/amina-isse/");
     })
 })
 const seeMyWork = document.querySelectorAll(".see-my-work");
 seeMyWork.forEach((item, index) => {
     item.addEventListener("click", arrow => {
-        location.href = "https://github.com/isse-amina"; 
+        window.open("https://github.com/isse-amina");
     })
 })
 const seeProject1 = document.querySelector(".see-project-1");
 seeProject1.onclick = function() {
-    location.href = "https://github.com/isse-amina/brain-tumor-detection"; 
+    window.open("https://github.com/isse-amina/brain-tumor-detection");
 }
 const seeProject2 = document.querySelector(".see-project-2");
 seeProject2.onclick = function() {
-    location.href = "https://github.com/isse-amina/bank-app/"; 
+    window.open("https://github.com/isse-amina/bank-app/");
 }
